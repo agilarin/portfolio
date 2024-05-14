b225586d4a948b5bcd660482489b8187434b20a8
Vladimir <grivle@mail.ru>
2024-05-14 11:39:56 +0700

    add TechnologiesItem and technologies icon

--- src/pages/Home/components/Technologies/TechnologiesItem.tsx
+++ src/pages/Home/components/Technologies/TechnologiesItem.tsx
@@ -1,11 +1,18 @@
+import React from "react";
 
 
+interface TechnologiesItemProps {
+  icon: ({className}: {className: string}) => React.ReactNode,
+  title: string
+}
 
-
-function TechnologiesItem() {
+function TechnologiesItem({icon, title}: TechnologiesItemProps) {
   return (
     <div className="flex flex-col items-center">
-      <p className="text-gray-600 text-lg dark:text-gray-300"></p>
+        {icon({className: "h-16"})}
+      <p className="text-gray-600 text-lg dark:text-gray-300">
+        {title}
+      </p>
     </div>
   );
 }
--- src/pages/Home/components/Technologies/index.tsx
+++ src/pages/Home/components/Technologies/index.tsx
@@ -1,8 +1,32 @@
+import TechnologiesItem from "@pages/Home/components/Technologies/TechnologiesItem.tsx";
+import IconJS from "@assets/icons/icon-javascript.svg?react";
+import IconTS from "@assets/icons/icon-typescript.svg?react";
+import IconReact from "@assets/icons/icon-react.svg?react";
+import IconRedux from "@assets/icons/icon-redux.svg?react";
+import IconSass from "@assets/icons/icon-sass.svg?react";
+import IconTailwind from "@assets/icons/icon-tailwind.svg?react";
+import IconNodejs from "@assets/icons/icon-nodejs.svg?react";
+import IconGit from "@assets/icons/icon-git.svg?react";
+
+
+
+const technologiesItemsProps = [
+  { title: "JavaScript", icon: ({className}: {className: string}) => <IconJS className={className}/> },
+  { title: "TypeScript", icon: ({className}: {className: string}) =>  <IconTS className={className}/> },
+  { title: "React", icon: ({className}: {className: string}) =>  <IconReact className={className}/> },
+  { title: "Redux", icon: ({className}: {className: string}) =>  <IconRedux className={className}/> },
+  { title: "Node.js", icon: ({className}: {className: string}) =>  <IconNodejs className={className}/> },
+  { title: "Sass/Scss", icon: ({className}: {className: string}) =>  <IconSass className={className}/> },
+  { title: "Tailwindcss", icon: ({className}: {className: string}) =>  <IconTailwind className={className}/> },
+  { title: "Git", icon: ({className}: {className: string}) =>  <IconGit className={className}/> },
+]
+
 
 function Technologies() {
+  console.log(technologiesItemsProps)
   return (
     <div className="dark:bg-gray-900">
-      <div className="container py-24 flex flex-col items-center">
+      <div className="container py-16 flex flex-col items-center sm:py-24">
 
         <div className="mb-4 py-1 px-5 bg-gray-200 rounded-xl dark:bg-gray-700">
           <h3 className="text-gray-600 font-medium text-sm dark:text-gray-300">
@@ -10,9 +34,20 @@ function Technologies() {
           </h3>
         </div>
 
-        <p className="text-gray-600 text-xl dark:text-gray-300">
+        <p className="mb-6 text-gray-600 text-xl text-center dark:text-gray-300 sm:mb-12">
           The skills, tools and technologies I am really good at:
         </p>
+
+
+        <div className="w-full grid gap-y-12 grid-cols-3 sm:grid-cols-5 lg:grid-cols-8">
+          {technologiesItemsProps.map(({title, icon}) =>
+            <TechnologiesItem
+              key={title}
+              title={title}
+              icon={icon}
+            />
+          )}
+        </div>
 
       </div>
     </div>
