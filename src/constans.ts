eb25fc52acdd0d277e3c5d44c6a20ed116800ee2
Vladimir <grivle@mail.ru>
2024-05-22 12:53:24 +0700

    add hash navigation

--- src/constans.ts
+++ src/constans.ts
@@ -1,5 +1,12 @@
 export const PATHS = {
   home: "/",
   projects: "/projects",
+}
 
+
+export const HOME_NAV = {
+  about: "about",
+  technologies: "technologies",
+  projects: "projects",
+  contacts: "contacts",
 }
