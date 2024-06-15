c7bbab7c492b9dbeeb3a050b5df7dd3373f6c6e8
Vladimir <grivle@mail.ru>
2024-06-15 18:44:41 +0700

    update home intro

--- src/pages/Home/components/Intro/index.tsx
+++ src/pages/Home/components/Intro/index.tsx
@@ -16,8 +16,8 @@ function Intro() {
             Junior Frontend Разработчик
           </h2>
           <p className="mb-12 text-gray-600 dark:text-gray-300">
-            I'm a full stack developer (React.js & Node.js) with a focus on creating (and occasionally designing) exceptional digital experiences that are fast, accessible,
-            visually appealing, and responsive. Even though I have been creating web applications for over 7 years, I still love it as if it was something new.
+
+
           </p>
           <div className="mb-12 flex items-center">
             <IconLocation className="mr-2 stroke-gray-600 dark:stroke-gray-300"/>
