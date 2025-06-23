705e02c9294b47302bf6be7da17f555911091713
Vladimir <grivle@mail.ru>
2025-06-23 15:15:56 +0700

    update data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,11 +1,10 @@
-import {Project} from "@/types";
+import { Project } from "@/types";
 // import tetrisImg from "@/assets/projects/tetris-640.png";
 import bookwormImage from "@/assets/projects/bookworm-640.jpg";
 import kinopoiskImage from "@/assets/projects/kinopoisk-640.jpg";
 import webchatImage from "@/assets/projects/webchat-640.png";
 
-
-export const projectsData : Project[] = [
+export const projectsData: Project[] = [
   {
     id: "4",
     name: "WebChat",
@@ -14,18 +13,27 @@ export const projectsData : Project[] = [
       "В приложении вы можете найти себе собеседника для общения и создать приватный чат для общения с ним.",
     img: webchatImage,
     tags: ["JavaScript", "TypeScript", "React", "Scss", "Firebase"],
-    url: "https://vlagris.github.io/webchat/",
-    github: "https://github.com/vlagris/webchat/"
+    url: "https://agilarin.github.io/webchat/",
+    github: "https://github.com/agilarin/webchat/",
   },
   {
     id: "3",
     name: "BookWorm",
-    description: "Веб-приложение интернет-магазина книг. " +
+    description:
+      "Веб-приложение интернет-магазина книг. " +
       "На нем можно найти интересующие вас книги и добавить их себе в корзину.",
     img: bookwormImage,
-    tags: ["JavaScript", "TypeScript", "React", "Redux", "Scss", "Firebase"],
-    url: "https://vlagris.github.io/bookworm-app/",
-    github: "https://github.com/vlagris/bookworm-app/"
+    tags: [
+      "JavaScript",
+      "TypeScript",
+      "React",
+      "Next.js",
+      "Redux",
+      "Scss",
+      "Firebase",
+    ],
+    url: "https://bookworm-agilarin.netlify.app/",
+    github: "https://github.com/agilarin/bookworm-app/",
   },
   {
     id: "2",
@@ -36,8 +44,8 @@ export const projectsData : Project[] = [
       "На сайте доступны списки фильмов, возможность поиска и карточки фильмов.",
     img: kinopoiskImage,
     tags: ["JavaScript", "TypeScript", "React", "Scss"],
-    url: "https://vlagris.github.io/kinopoisk-clone/",
-    github: "https://github.com/vlagris/kinopoisk-clone/"
+    url: "https://agilarin.github.io/kinopoisk-clone/",
+    github: "https://github.com/agilarin/kinopoisk-clone/",
   },
   // {
   //   id: "1",
@@ -48,4 +56,4 @@ export const projectsData : Project[] = [
   //   url: "https://vlagris.github.io/tetris-app/",
   //   github: "https://github.com/vlagris/tetris-app/"
   // }
-]
+];
